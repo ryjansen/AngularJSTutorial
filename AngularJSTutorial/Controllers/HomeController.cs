a3a6ab5fd24dfa0524f3c90df7dab9cfb34b9142
unknown <ryjansen@ryjansen-laptop.redmond.corp.microsoft.com>
2015-03-23 08:37:14 -0700

    Add ListWebsites mock endpoint

--- AngularJSTutorial/Controllers/HomeController.cs
+++ AngularJSTutorial/Controllers/HomeController.cs
@@ -26,5 +26,23 @@ namespace AngularJSTutorial.Controllers
 
             return View();
         }
+
+        [HttpGet]
+        public JsonResult ListWebsites()
+        {
+            var websites = new List<Website>();
+            websites.Add(new Website() { Name = "website1", Status = "Running" });
+            websites.Add(new Website() { Name = "website2", Status = "Running" });
+            websites.Add(new Website() { Name = "website3", Status = "Stopped" });
+            websites.Add(new Website() { Name = "website4", Status = "Quota Exceeded" });
+            websites.Add(new Website() { Name = "website5", Status = "Warning" });
+            return this.Json(websites, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    public class Website
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
     }
 }

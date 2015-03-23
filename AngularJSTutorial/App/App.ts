4751b5f4fdb85e68fa833858d3c7496b0d230a83
Ryan Jansen (AZURE) <ryjansen@microsoft.com>
2015-03-23 08:07:39 -0700

    Initial commit

---
+++ AngularJSTutorial/App/App.ts
@@ -0,0 +1,18 @@
+﻿module App {
+    var app = angular.module("app", [
+        "ngRoute"
+    ]);
+
+    app.config(["$routeProvider", ($routeProvider: ng.route.IRouteProvider) => {
+        $routeProvider
+            .when("/home", {
+                templateUrl: "/App/Controllers/HomeView.html",
+                controller: "homeController",
+            })
+            .otherwise({
+                redirectTo: "/home"
+            });
+    }]);
+
+    app.run(() => { });
+}
---
+++ AngularJSTutorial/App/Controllers/HomeController.ts
@@ -0,0 +1,12 @@
+﻿/// <reference path="..\App.ts"/>
+
+module App {
+
+    interface HomeScope {
+        message: string;
+    }
+
+    angular.module("app").controller("homeController", ($scope: HomeScope) => {
+        $scope.message = "Hello world!";
+    });
+}
